56ca9aa148b15a0831ec2c9baef5d82922d7a83e
Valentin Ackva <valentin.ackva@gmail.com>
2025-11-18 19:40:03 +0100

    updated binary data and core.h include

--- cmake/binary_data.cmake
+++ cmake/binary_data.cmake
@@ -18,11 +18,9 @@ function(tanh_add_binary_data target)
     endif()
 
     add_library(${target} STATIC)
-
     set_target_properties(${target} PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
 
     set(tanh_binary_data_folder "${CMAKE_CURRENT_BINARY_DIR}/tanh_binarydata_${target}/TanhLibraryCode")
-
     file(MAKE_DIRECTORY ${tanh_binary_data_folder})
 
     if(NOT TANH_ARG_NAMESPACE)
@@ -36,9 +34,19 @@ function(tanh_add_binary_data target)
     set(binary_data_header "${tanh_binary_data_folder}/${TANH_ARG_HEADER_NAME}")
     set(binary_data_sources "")
 
+    set(registry_variable_names "")
+    set(registry_file_names "")
+
     set(header_content "// Auto-generated binary data file\n")
     set(header_content "${header_content}#pragma once\n\n")
     set(header_content "${header_content}namespace ${TANH_ARG_NAMESPACE}\n{\n")
+
+    set(header_content "${header_content}    // Retrieves binary data by original filename (returns nullptr if not found)\n")
+    set(header_content "${header_content}    const char* getNamedResource(const char* fileName, int& sizeInBytes);\n\n")
+
+    set(header_content "${header_content}    extern const char* namedResourceList[];\n")
+    set(header_content "${header_content}    extern const char* originalFilenames[];\n")
+    set(header_content "${header_content}    extern const int   resourceCount;\n\n")
 
     set(file_index 1)
     foreach(source_file IN LISTS TANH_ARG_SOURCES)
@@ -47,7 +55,10 @@ function(tanh_add_binary_data target)
         get_filename_component(file_name "${source_file}" NAME)
         string(MAKE_C_IDENTIFIER "${file_name}" var_name)
 
-        set(header_content "${header_content}    extern const char*   ${var_name};\n")
+        list(APPEND registry_variable_names "${var_name}")
+        list(APPEND registry_file_names "${file_name}")
+
+        set(header_content "${header_content}    extern const char* ${var_name};\n")
         set(header_content "${header_content}    const int            ${var_name}Size = ")
 
         file(SIZE "${source_file}" file_size)
@@ -102,6 +113,59 @@ function(tanh_add_binary_data target)
 
     set(header_content "${header_content}}\n")
     file(WRITE "${binary_data_header}" "${header_content}")
+
+    set(registry_cpp "${tanh_binary_data_folder}/BinaryDataRegistry.cpp")
+    list(APPEND binary_data_sources "${registry_cpp}")
+
+    set(reg_content "// Auto-generated registry file\n")
+    set(reg_content "${reg_content}#include \"${TANH_ARG_HEADER_NAME}\"\n")
+    set(reg_content "${reg_content}#include <cstring>\n\n")
+    set(reg_content "${reg_content}namespace ${TANH_ARG_NAMESPACE}\n{\n")
+
+    set(reg_content "${reg_content}    // Internal arrays for lookup\n")
+    set(reg_content "${reg_content}    static const char* const dataPointers[] = {\n")
+    foreach(var_name IN LISTS registry_variable_names)
+        set(reg_content "${reg_content}        ${var_name},\n")
+    endforeach()
+    set(reg_content "${reg_content}        nullptr\n    };\n\n")
+
+    set(reg_content "${reg_content}    static const int dataSizes[] = {\n")
+    foreach(var_name IN LISTS registry_variable_names)
+        set(reg_content "${reg_content}        ${var_name}Size,\n")
+    endforeach()
+    set(reg_content "${reg_content}        0\n    };\n\n")
+
+    set(reg_content "${reg_content}    // Public Arrays\n")
+    set(reg_content "${reg_content}    const char* namedResourceList[] = {\n")
+    foreach(var_name IN LISTS registry_variable_names)
+        set(reg_content "${reg_content}        \"${var_name}\",\n")
+    endforeach()
+    set(reg_content "${reg_content}        nullptr\n    };\n\n")
+
+    set(reg_content "${reg_content}    const char* originalFilenames[] = {\n")
+    foreach(file_name IN LISTS registry_file_names)
+        set(reg_content "${reg_content}        \"${file_name}\",\n")
+    endforeach()
+    set(reg_content "${reg_content}        nullptr\n    };\n\n")
+
+    list(LENGTH registry_variable_names num_resources)
+    set(reg_content "${reg_content}    const int resourceCount = ${num_resources};\n\n")
+
+    set(reg_content "${reg_content}    const char* getNamedResource(const char* fileName, int& sizeInBytes)\n    {\n")
+    set(reg_content "${reg_content}        for(int i = 0; i < resourceCount; ++i)\n")
+    set(reg_content "${reg_content}        {\n")
+    set(reg_content "${reg_content}            if(std::strcmp(originalFilenames[i], fileName) == 0)\n")
+    set(reg_content "${reg_content}            {\n")
+    set(reg_content "${reg_content}                sizeInBytes = dataSizes[i];\n")
+    set(reg_content "${reg_content}                return dataPointers[i];\n")
+    set(reg_content "${reg_content}            }\n")
+    set(reg_content "${reg_content}        }\n")
+    set(reg_content "${reg_content}        sizeInBytes = 0;\n")
+    set(reg_content "${reg_content}        return nullptr;\n")
+    set(reg_content "${reg_content}    }\n")
+
+    set(reg_content "${reg_content}}\n")
+    file(WRITE "${registry_cpp}" "${reg_content}")
 
     target_sources(${target} PRIVATE ${binary_data_sources})
     target_include_directories(${target} PUBLIC ${tanh_binary_data_folder})
