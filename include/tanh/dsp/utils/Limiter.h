950b4039d2cd235984b3af440b57d26cd3d9808d
Valentin Ackva <valentin.ackva@gmail.com>
2026-02-17 13:36:09 +0100

    added simple limiter

---
+++ include/tanh/dsp/utils/Limiter.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <tanh/dsp/BaseProcessor.h>
+
+namespace thl::dsp::utils {
+
+class LimiterImpl : public BaseProcessor {
+public:
+    LimiterImpl();
+    ~LimiterImpl() override;
+
+    void prepare(const double& sample_rate, const size_t& samples_per_block, const size_t& num_channels) override;
+    void process(float** buffer, const size_t& num_samples, const size_t& num_channels) override;
+
+protected:
+    enum Parameter {
+        ThresholdGain = 0,
+
+        NUM_PARAMETERS = 1
+    };
+
+    template<typename T>
+    T get_parameter(Parameter parameter);
+
+    virtual float get_parameter_float(Parameter parameter) = 0;
+    virtual bool get_parameter_bool(Parameter parameter) = 0;
+    virtual int get_parameter_int(Parameter parameter) = 0;
+
+private:
+    double m_sample_rate = 48000.0;
+    size_t m_channels = 2;
+
+    static float tanh_limit(float input, float threshold);
+};
+
+template<> inline float LimiterImpl::get_parameter<float>(Parameter p) { return get_parameter_float(p); }
+template<> inline bool LimiterImpl::get_parameter<bool>(Parameter p) { return get_parameter_bool(p); }
+template<> inline int LimiterImpl::get_parameter<int>(Parameter p) { return get_parameter_int(p); }
+
+} // namespace thl::dsp::utils
