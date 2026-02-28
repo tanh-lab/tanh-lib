b650948a3bf2f24f913773fb16b4549b5f61debd
Fares Schulz <schulzfares@gmail.com>
2026-02-28 23:51:08 +0100

    Updated SineProcessor to new dsp format

--- src/dsp/synth/SineProcessor.cpp
+++ src/dsp/synth/SineProcessor.cpp
@@ -6,10 +6,10 @@ using namespace thl::dsp::synth;
     #define M_PI 3.14159265358979323846
 #endif
 
-SineProcessor::SineProcessor() = default;
-SineProcessor::~SineProcessor() = default;
+SineProcessorImpl::SineProcessorImpl() = default;
+SineProcessorImpl::~SineProcessorImpl() = default;
 
-void SineProcessor::prepare(const double& sample_rate, const size_t& samples_per_block, const size_t& num_channels) {
+void SineProcessorImpl::prepare(const double& sample_rate, const size_t& samples_per_block, const size_t& num_channels) {
     m_phase.resize(num_channels, 0.f);
 
     m_sample_rate = sample_rate;
@@ -18,13 +18,16 @@ void SineProcessor::prepare(const double& sample_rate, const size_t& samples_per
     smoothed_frequency.reset(sample_rate, 0.005, utils::SmoothedValue::Linear);
     smoothed_amplitude.reset(sample_rate, 0.005, utils::SmoothedValue::Linear);
 
-    smoothed_frequency.set_current_and_target_value(m_frequency);
-    smoothed_amplitude.set_current_and_target_value(m_amplitude);
+    smoothed_frequency.set_current_and_target_value(get_parameter<float>(Frequency));
+    smoothed_amplitude.set_current_and_target_value(get_parameter<float>(Amplitude));
 }
 
-void SineProcessor::process(float** buffer, const size_t& num_samples, const size_t& num_channels) {
+void SineProcessorImpl::process(float** buffer, const size_t& num_samples, const size_t& num_channels) {
         constexpr float two_pi = 2.0f * static_cast<float>(M_PI);
         const auto sample_rate_f = static_cast<float>(m_sample_rate);
+
+        smoothed_frequency.set_target_value(get_parameter<float>(Frequency));
+        smoothed_amplitude.set_target_value(get_parameter<float>(Amplitude));
 
         for (size_t i = 0; i < num_samples; ++i) {
             const float current_freq = smoothed_frequency.get_smoothed_value(1);
@@ -45,37 +48,3 @@ void SineProcessor::process(float** buffer, const size_t& num_samples, const siz
             }
         }
     }
-
-void SineProcessor::set_parameter(Parameters param, float value) {
-    set_parameter(static_cast<int>(param), value);
-}
-
-float SineProcessor::get_parameter(Parameters param) const {
-    return get_parameter(static_cast<int>(param));
-}
-
-void SineProcessor::set_parameter(int param_id, float value) {
-    switch (param_id) {
-        case FREQUENCY:
-            m_frequency = value;
-            smoothed_frequency.set_target_value(m_frequency);
-            break;
-        case AMPLITUDE:
-            m_amplitude = value;
-            smoothed_amplitude.set_target_value(m_amplitude);
-            break;
-        default:
-            break;
-    }
-}
-
-float SineProcessor::get_parameter(int param_id) const {
-    switch (param_id) {
-        case FREQUENCY:
-            return m_frequency;
-        case AMPLITUDE:
-            return m_amplitude;
-        default:
-            return 0.f;
-    }
-}
