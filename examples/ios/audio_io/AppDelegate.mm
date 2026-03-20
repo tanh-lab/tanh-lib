eff41f2246066786eb4bf1fe5e8d3cd87742e76a
Fares Schulz <schulzfares@gmail.com>
2026-03-20 18:02:50 +0000

    Added std::cout and std::err logging for the audio_io example!

--- examples/ios/audio_io/AppDelegate.mm
+++ examples/ios/audio_io/AppDelegate.mm
@@ -1,10 +1,22 @@
 #import "AppDelegate.h"
 #import "ViewController.h"
+#import <tanh/core/Logger.h>
+#import <iostream>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    thl::Logger::set_callback([](const thl::Logger::LogRecord& record) {
+        auto level = static_cast<thl::Logger::LogLevel>(record.level);
+        std::string line = thl::Logger::format_plain(record);
+        if (level <= thl::Logger::LogLevel::Warning) {
+            std::cerr << line << std::endl;
+        } else {
+            std::cout << line << std::endl;
+        }
+    });
+
     _window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     _window.rootViewController = [[ViewController alloc] init];
     [_window makeKeyAndVisible];
